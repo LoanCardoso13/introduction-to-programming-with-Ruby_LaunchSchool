45fb3f46156eeb6d1886bc62b905e3d80456d832
Loan Cardoso <jow13_122@yahoo.com>
2023-08-19 10:06:22 -0400

    Third chapter concluded.

---
+++ Methods/returns.rb
@@ -0,0 +1,28 @@
+=begin
+1. x = 2
+
+2. puts x = 2
+
+3. p name = "Joe"
+
+4. four = "four"
+
+5. print something = "nothing"
+=end
+
+e1 = x = 2
+
+e2 = puts x = 2
+
+e3 = p name = "Joe"
+
+e4 = four = "four"
+
+e5 = print something = "nothing"
+
+print "\n\n\n\n"
+puts "x = 2 returns #{e1}"
+puts "puts x = 2 returns #{e2}"
+puts "p name = 'Joe' returns #{e3}"
+puts "four = 'four' returns #{e4}"
+puts "print something = 'nothing' returns #{e5}"
